db6cd02e314e7e94b169c345d8e6da2db31f9295
Mark Lawlor <mwlawlor@gmail.com>
2023-09-29 08:39:06 +1000

    fix tests

--- packages/expo-router/src/global-state/routing.ts
+++ packages/expo-router/src/global-state/routing.ts
@@ -140,20 +140,22 @@ function getNavigateAction(state: ResultState, rootState: NavigationState) {
   };
 }
 
-function getNavigatePushAction(desiredState: ResultState, navigationState: NavigationState) {
-  const sharedParents = getSharedNavigators(desiredState, navigationState, false);
+function getNavigatePushAction(desiredState: ResultState, rootState: NavigationState) {
+  const sharedParents = getSharedNavigators(desiredState, rootState, false);
+  const lastSharedParent = sharedParents.at(-1);
 
-  if (sharedParents && sharedParents.at(-1)?.type === 'stack') {
+  if (lastSharedParent?.type === 'stack') {
     const { screen, params } = rewriteNavigationStateToParams(desiredState);
     return {
       type: 'PUSH',
+      target: lastSharedParent.key,
       payload: {
         name: screen,
         params,
       },
     };
   } else {
-    return getNavigateAction(desiredState, navigationState);
+    return getNavigateAction(desiredState, rootState);
   }
 }
 
@@ -210,5 +212,5 @@ function getSharedNavigators(
     return shared;
   }
 
-  return undefined;
+  return [];
 }
